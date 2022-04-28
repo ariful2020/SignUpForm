2889590bb1814451a0182195f17ac8a6bb8dd302
Ariful Islam Noyon <70486157+ariful2020@users.noreply.github.com>
2022-04-28 06:32:18 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+/*var array = new Array(
+  "Sushanta",
+  "Prashanta",
+  "Others",
+  [
+    "Sushanta is a student of Eleven.",
+    "Prashanta is a student of One",
+    "They are others",[
+      "Nice",
+      "Good",
+      "Excelent"
+    ]
+  ]
+)
+console.log(array[3][0]);
+var addj = array[3][3][2];
+
+function name(name){
+  return "Hello " + name + addj;
+}
+var myname =  name;
+var names = myname("Sushanta ");
+console.log(names);
+console.log(names.length);
+var newArray = [];
+newArray[0] = "Orange";
+newArray[1] = "Mango";
+newArray[2] = [
+  "Bela",
+  "Rose"
+];
+console.log(newArray[2][1])*/
